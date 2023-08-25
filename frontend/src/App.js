474ea81c5c9fdc49cd12d5ec84548a637de62c7d
paulaji <paulajiparayil123@gmail.com>
2023-08-25 16:44:39 +0530

    router, routes and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
 
+// react router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
     <div className="App">
-      <p>Namaskaaram!</p>
+      <Router>
+        <Routes>
+          <Route Component={HomePage} path="/" exact />
+          <Route Component={LoginPage} path="/login" />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" name="username" placeholder="username" />
+        <input type="password" name="password" placeholder="password" />
+        <button type="submit">login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
