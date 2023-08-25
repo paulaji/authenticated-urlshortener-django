a25c3c8abd232db4a40e7db8634cfe1cd1dd64c0
paulaji <paulajiparayil123@gmail.com>
2023-08-25 13:38:37 +0530

    blacklisted refreshtoken after rotation

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'application',
     # include restframework (DRF)
     'rest_framework',
+    # to blacklist a refresh token
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 # to use simplejwt authentication
@@ -56,8 +58,10 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": False,
-    "BLACKLIST_AFTER_ROTATION": False,
+    # setting refresh token rotation to true for more security
+    "ROTATE_REFRESH_TOKENS": True,
+    # blacklisting once used refresh tokens
+    "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
 
     "ALGORITHM": "HS256",
