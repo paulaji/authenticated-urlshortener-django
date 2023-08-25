c77564119dca9fbde2a16b74f3530feff053943f
paulaji <paulajiparayil123@gmail.com>
2023-08-25 16:54:06 +0530

    jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,18 @@ import "./App.css";
 // react router
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// pages import
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+
+// components import
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route Component={HomePage} path="/" exact />
           <Route Component={LoginPage} path="/login" />
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+    </>
+  );
+};
+
+export default Header;
