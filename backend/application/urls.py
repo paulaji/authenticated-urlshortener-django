be25f7dc94dd66d46ae9e265225fdf5159f75bb1
Paul Aji <paulajiparayil123@gmail.com>
2023-08-26 23:43:53 +0530

    Update urls.py

--- backend/application/urls.py
+++ backend/application/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register_user, name="register"),
+    path('registeruser/', views.register_user, name="register"),
 ]
