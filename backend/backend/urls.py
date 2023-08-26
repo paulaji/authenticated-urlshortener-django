68aa59576434be1f9680c933b04e54c2ced4f693
Paul Aji <paulajiparayil123@gmail.com>
2023-08-26 22:44:59 +0530

    register user

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # setting the path to urls.py in the newly created api folder inside application
-    path('api/', include('application.api.urls'))
+    path('api/', include('application.api.urls')),
+    # manually writing a register user functionality, we can always use the django admin panel to add more users
+    path('register/', views.register_user, name="register"),
 ]
