fafd2e1184a84076bff411cafad9446299504df8
Paul Aji <paulajiparayil123@gmail.com>
2023-08-26 22:31:52 +0530

    Update serializers.py

--- backend/application/api/serializers.py
+++ backend/application/api/serializers.py
@@ -1,9 +1,11 @@
-from rest_framework.serializers import ModelSerializer
-# import the Note model
-from ..models import Note
+# incase a basic note section is to be made
 
-# serialize the Note model
-class NoteSerializer(ModelSerializer):
-    class Meta:
-        model = Note
-        fields = '__all__'
+# from rest_framework.serializers import ModelSerializer
+# # import the Note model
+# from ..models import Note
+
+# # serialize the Note model
+# class NoteSerializer(ModelSerializer):
+#     class Meta:
+#         model = Note
+#         fields = '__all__'
