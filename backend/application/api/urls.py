2eee74e8bf33e0a562ca3bf954efde23857e1eb2
Paul Aji <paulajiparayil123@gmail.com>
2023-08-26 22:32:12 +0530

    Update urls.py

--- backend/application/api/urls.py
+++ backend/application/api/urls.py
@@ -12,8 +12,8 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     # in the root url, implement the getRoutes function
     path('', views.getRoutes),
-    # url to access the serialized notes
-    path('notes/', views.getNotes),
+    # url to access the serialized notes incase notes url is made
+    # path('notes/', views.getNotes),
     # urls to access the built-in views of simplejwt
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'), # here, we display the customized token
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
