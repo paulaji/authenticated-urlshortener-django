f2020c91d81e482bbc5a5fbf218c5415c5c01a38
Paul Aji <paulajiparayil123@gmail.com>
2023-08-26 22:35:43 +0530

    Update LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,17 +1,57 @@
-import { React, useContext } from "react";
+import React, { useContext } from "react";
 import AuthContext from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-  // using the useContext hook to access the loginUser function from AuthContext
   let { loginUser } = useContext(AuthContext);
+
   return (
-    <div>
-      {/* form on submit will go to the login user function defined in the AuthContext which will submit the username and password from this form and validate this in the backend connection, port8000 and if credentials match, it will return access and refresh tokens */}
-      <form onSubmit={loginUser}>
-        <input type="text" name="username" placeholder="username" />
-        <input type="password" name="password" placeholder="password" />
-        <button type="submit">login</button>
-      </form>
+    <div className="container d-flex justify-content-center align-items-center min-vh-100">
+      <div className="card shadow p-4">
+        <h3 className="mb-4 text-center">
+          Django-React | JWT | Authenticator{" "}
+        </h3>
+        <h5 className="mb-4 text-center">Login</h5>
+        <form onSubmit={loginUser}>
+          <div className="mb-3">
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              placeholder="Username"
+            />
+          </div>
+          <div className="mb-3">
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="d-flex justify-content-center mb-3">
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </form>
+        <div className="text-center">
+          <p>
+            Don't have an account? <Link to="/register">Register</Link>
+          </p>
+          <p>Or</p>
+          <small>Use the following test account(s):</small>
+          <br />
+          <small>
+            username: <span style={{ color: "blue" }}>user1</span> or{" "}
+            <span style={{ color: "blue" }}>user2</span>
+          </small>
+          <br />
+          <small>
+            username: <span style={{ color: "blue" }}>resu321321</span>
+          </small>
+        </div>
+      </div>
     </div>
   );
 };
