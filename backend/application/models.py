9c955efb3e6a4680952ebb036ef3916a78d05061
Paul Aji <paulajiparayil123@gmail.com>
2023-08-26 22:33:20 +0530

    Update models.py

--- backend/application/models.py
+++ backend/application/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # import the user object
 from django.contrib.auth.models import User
 
-# Create your models here.
-# note model
-class Note(models.Model):
-    # our user will be the ForeignKey used to access notes data/table
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    body = models.TextField(default="sample note")
+# incase note model is to be made
+# # Create your models here.
+# # note model
+# class Note(models.Model):
+#     # our user will be the ForeignKey used to access notes data/table
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+#     body = models.TextField(default="sample note")
