b745ced1040e568dfa1057aeb308d36b9e97de6a
Paul Aji <paulajiparayil123@gmail.com>
2023-08-27 08:57:38 +0530

    shortened url view

--- backend/application/views.py
+++ backend/application/views.py
@@ -21,3 +21,25 @@ def register_user(request):
     
     # Always render the template with the form
     return render(request, 'register.html', {'form': form})
+
+# url shortening
+from .models import ShortenedURL
+
+# to pass in long url and retrieve shortened url
+def shorten_url(request):
+    if request.method == 'POST':
+        long_url = request.POST['long_url']
+        shortened_url = ShortenedURL.objects.create(long_url=long_url)
+        return render(request, 'shorten_url.html', {'shortened_url': shortened_url})
+
+    return render(request, 'shorten_url.html')
+
+# for redirection logic
+# check if shortened url matches the long url and redirect to long url
+def redirect_to_original(request, short_code):
+    try:
+        shortened_url = ShortenedURL.objects.get(short_code=short_code)
+        return redirect(shortened_url.long_url)
+    except ShortenedURL.DoesNotExist:
+        error_message = "Please try again."
+        return render(request, 'shorten_url.html', {'error_message': error_message})  
